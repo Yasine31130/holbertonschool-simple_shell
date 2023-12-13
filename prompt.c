bd12fcf6ddc939c2dba1e19fb57dd02485901340
mathpvx <mathilde.pavaux@gmail.com>
2023-12-13 02:29:29 -0800

    update prompt

--- prompt.c
+++ prompt.c
@@ -1,12 +1,15 @@
 #include "main.h"
 /**
- *
+ * main - prints "$", wait for the user to enter a cmd and prints it on the next line
+ * Return: 0 if success
  */
-int main(void)
+int main()
 {
 	char * line = 0;
 	size_t buffer_size = 0;
 	ssize_t bytes_read;
+	const char * delimiter = " ";
+	char ** array;
 
 	while (1)
 	{
@@ -14,7 +17,7 @@ int main(void)
 		bytes_read = getline(&line, &buffer_size, stdin);
 
 		if (bytes_read != -1)
-			printf("%s", line);
+			array = splitString(line, delimiter);
 	}
 
 	free (line);

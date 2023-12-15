294746213ad3cf12b9cf3607c23365c015425b57
mathpvx <mathilde.pavaux@gmail.com>
2023-12-15 03:31:58 -0800

    executes execve in the child process

--- exec.c
+++ exec.c
@@ -6,13 +6,23 @@ int main ()
 {
 	char * argv[] = {"/bin/ls", "-l", NULL};
 	int val;
+	pid_t pid;
 	
-	val = execve(argv[0], argv, NULL);
+	pid = fork ();
 
-	if (val == -1)
-		perror("Error");
+	if (pid == -1)
+		return (-1);
 
-	printf("Done");
+	if (pid == 0)
+	{
+		val = execve(argv[0], argv, NULL);
+
+		if (val == -1)
+			perror("Error");
+	}
+
+	wait(NULL);
+	printf("After execve\n");
 
 	return (0);
 }

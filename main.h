a1e66780307c6690538557a0612f5b8f3de66939
mathpvx <mathilde.pavaux@gmail.com>
2023-12-15 12:13:56 -0800

    add prototype

--- main.h
+++ main.h
@@ -3,5 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 char ** splitString(char * str, const char * delimiter);
+int print_ppid(void);
 #endif

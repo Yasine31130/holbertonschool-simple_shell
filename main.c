c9a94a4debba5e139534bee39d5269d321c4f811
mathpvx <mathilde.pavaux@gmail.com>
2023-12-18 00:22:32 -0800

    main function

---
+++ main.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ *
+ */
+int main()
+{
+	char * cmd = NULL;
+	size_t cmd_len = 0;
+	ssize_t bytes_read;
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+		{
+			printf("$ ");
+			fflush(stdout);
+		}
+
+		bytes_read = getline(&cmd, &cmd_len, stdin);
+
+		if (bytes_read == -1)
+		{
+			printf("\n");
+			free(cmd);
+			exit(EXIT_SUCCESS);
+		}
+
+		/**Remove extra linebreak when user press enter**/
+		if (cmd[bytes_read - 1] == '\n') {
+			cmd[bytes_read - 1] = '\0';
+		}
+
+		get_args(cmd);
+
+		/**Free the memory allocated by getline**/
+		free(cmd);
+		cmd = NULL;
+		cmd_len = 0;
+	}
+
+	return 0;
+}

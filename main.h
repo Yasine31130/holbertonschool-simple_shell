2d19459440061441cb910f98831e76463b53a11d
mathpvx <mathilde.pavaux@gmail.com>
2023-12-19 01:39:13 -0800

    getenv prototype double written fixed

--- main.h
+++ main.h
@@ -10,7 +10,6 @@
 #define MAX_ARGS 32
 extern char **environ;
 void execute_cmd(char **args);
-char *_getenv(const char *name);
 void execute_command(char **args);
 void search_in_path(char **args, char *path);
 void _printenv(void);
